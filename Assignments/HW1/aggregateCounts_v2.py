a22ffff20c4c01ac36f987a57af0dc24fe8d295a
luisvillarreal <luis.villarreal@berkeley.edu>
2021-08-25 18:44:49 -0500

    Adding HW1

---
+++ Assignments/HW1/aggregateCounts_v2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+This script reads word counts from STDIN and aggregates
+the counts for any duplicated words.
+
+INPUT & OUTPUT FORMAT:
+    word \t count
+USAGE (standalone):
+    python aggregateCounts_v2.py < yourCountsFile.txt
+
+Instructions:
+    For Q7 - Your solution should not use a dictionary or store anything   
+             other than a single total count - just print them as soon as  
+             you've added them. HINT: you've modified the framework script 
+             to ensure that the input is alphabetized; how can you 
+             use that to your advantage?
+"""
+
+# imports
+import sys
+
+
+################# YOUR CODE HERE #################
+
+
+
+
+
+
+
+
+
+
+
+
+################ (END) YOUR CODE #################
